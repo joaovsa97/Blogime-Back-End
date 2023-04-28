671df31cb7a4685a297312df557d9f099a68a6fe
joaovsa97 <joaos.azevedo10@gmail.com>
2023-04-28 14:06:32 -0300

    create: register function(not working - CORS error)

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
-import authRoutes from "./routes/auth.js"
-import postsRoutes from "./routes/posts.js"
-import userRoutes from "./routes/user.js"
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import postsRoutes from "./routes/posts.js";
+import userRoutes from "./routes/user.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/posts", authRoutes)
-app.use("/posts", postsRoutes)
-app.use("/user", userRoutes)
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+app.use(cors({ origin: "*" }));
 
 app.listen(8800, () => {
-    console.log("connected")
-})
+  console.log("connected");
+});
